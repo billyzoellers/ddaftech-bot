2c2e7f5c2bf2c8c9a0feb651a11210015ca9041b
Billy Zoellers <billy.zoellers@mac.com>
2019-08-07 04:38:14 +0000

    added ipinfo tools

---
+++ features/tools-rdns.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = function(controller) {
+
+    controller.hears(new RegExp(/^\/tools rdns (.*?)$/),'message,direct_message', async(bot, message) => {
+        
+        // connect to the ipinfo.io API
+        const api = require("ipinfo");
+        
+        console.log("tools-ip.js: user requested " + message.matches[1])
+        var ip = message.matches[1];
+        var token = process.env.IPINFO_TOKEN;
+        var text = "text";
+        
+        // create a Promise wrapper around this API
+        let callAPI = (ip) => {
+            return new Promise((resolve, reject) => {
+                api(ip, token, (err, cLoc) => {
+                    
+                    if (err) {
+                        reject(err);
+                    }
+        
+                    resolve(cLoc);
+                });
+            })
+        };
+        
+        // wait for API, then respond
+        let result = await callAPI(ip);
+        if (result.ip == undefined) {
+            text = "I was not able to find information about that IP address."
+        } else {
+            text = result.ip + " resolves to " + result.hostname + ".";
+        }
+
+        await bot.reply(message, {markdown: text});
+        
+    // controller
+    });
+
+}
