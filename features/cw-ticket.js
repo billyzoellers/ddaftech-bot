3fdc787b0c97c1736f4f10fcf19dffa2d1a0f281
Billy Zoellers <billy.zoellers@mac.com>
2019-08-08 02:54:50 +0000

    regex to match operation for /cw ticket

--- features/cw-ticket.js
+++ features/cw-ticket.js
@@ -5,10 +5,12 @@
 
 module.exports = function(controller) {
 
-    controller.hears(new RegExp(/^\/cw ticket ([0-9]*?)$/),'message,direct_message', async(bot, message) => {
+    controller.hears(new RegExp(/^\/cw ticket (\S+)(?:$|\s)($|\S+)/),'message,direct_message', async(bot, message) => {
         
         let ticketId = message.matches[1];
-        console.log('/cw-ticket.js: requested ticket ' + ticketId);
+        let operation = message.matches[2];
+        
+        console.log('/cw-ticket.js: requested ticket ' + ticketId + ' operation ' + operation);
         
         // Connect to CW API
         const ConnectWiseRest = require('connectwise-rest');
@@ -198,71 +200,6 @@ module.exports = function(controller) {
         
         
         await bot.reply(message, {markdown: text, attachments: card});
-        
-    // controller
-    });
-    
-    controller.hears(new RegExp(/^\/cw ticket ([0-9]*?) (.*?)$/),'message,direct_message', async(bot, message) => {
-        
-        let ticketId = message.matches[1];
-        let operation = message.matches[2];
-        
-        console.log('/cw-ticket.js: requested ticket ' + ticketId + " operation " + operation);
-        
-        // Connect to CW API
-        const ConnectWiseRest = require('connectwise-rest');
-        const cw = new ConnectWiseRest({
-            companyId: process.env.CW_COMPANY,
-            companyUrl: 'connectwise.deandorton.com',
-            publicKey: process.env.CW_PUBLIC_KEY,
-            privateKey: process.env.CW_PRIVATE_KEY,
-            debug: false,               // optional, enable debug logging
-            logger: (level, text, meta) => { } // optional, pass in logging function
-        });
-        
-        let ticket = await cw.ServiceDeskAPI.Tickets.getTicketById(ticketId);
-        let serviceNotes = await cw.ServiceDeskAPI.ServiceNotes.getServiceNotes(ticketId);
-        
-        let text = "<blockquote><h3>Ticket " + ticket.id +  " - " + ticket.summary + "</h3>";
-        
-        text += "<strong>Status:</strong> " + ticket.status.name;
-        
-        text += "<br><strong>Requester:</strong> <a href='mailto:" + ticket.contactEmailAddress + "'>" + ticket.contactName + "</a> at " + ticket.company.name;
-        
-        if (ticket.owner) {
-            text += "<br><strong>Assignee:</strong> " + ticket.owner.name + " (" + ticket.board.name + ")";
-        }        
-
-        if (serviceNotes) {
-            text += "<hr>"
-        }
-        
-        // add text for each service note
-        for(let note of serviceNotes) {
-            let formattedNote = note.text.replace(/\n/g, '<br>')
-            
-            text += "<strong>"
-            
-            if (note.contact) {
-                text += note.contact.name;
-            }
-            if (note.member) {
-                text += note.member.name;
-            }
-            
-            text += " on " + dateToHumanReadable(new Date(note.dateCreated)) + "</strong>";
-            
-            if (note.internalFlag) {
-                text += " Internal Note"
-            }
-            
-            text += "<blockquote>" + formattedNote + "</blockquote>";
-  
-        }
-        
-        text += "</blockquote>";
-
-        await bot.reply(message, {markdown: text});
         
     // controller
     });
